88069693a9f1540289644a3faa08302e5a3f6461
Rafael Santos Teixeira <rafaelsantost87@gmail.com>
2025-08-11 10:06:59 -0300

    inflow e outflow

--- outflows/views.py
+++ outflows/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, CreateView, DetailView
+from . import models, forms
+from django.urls import reverse_lazy
+
+
+class OutflowListView(ListView):
+    model = models.Outflow
+    template_name = 'outflow_list.html'
+    context_object_name = 'outflows'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        product = self.request.GET.get('product')
+
+        if product:
+            queryset = queryset.filter(product__title__icontains=product)
+        
+        return queryset
+
+
+class OutflowCreateView(CreateView):
+    models = models.Outflow
+    template_name = 'outflow_create.html'
+    form_class = forms.OutflowForm
+    success_url = reverse_lazy('outflow_list')
+
+
+class OutflowDetailView(DetailView):
+    model = models.Outflow
+    template_name = 'outflow_detail.html'
