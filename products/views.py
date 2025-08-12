99ee21956251b3709bb6c7fa80f33ec564e3d4a3
Rafael Santos <rafaelsantost87@gmail.com>
2025-08-12 10:57:07 -0300

    post save de quantity de inflows e outflows

--- products/views.py
+++ products/views.py
@@ -14,12 +14,24 @@ class ProductListView(ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         title = self.request.GET.get('title')
+        serie_number = self.request.GET.get('serie_number')
+        category = self.request.GET.get('category')
+        brand = self.request.GET.get('brand')
 
         if title:
             queryset = queryset.filter(title__icontains=title)
-        
+
+        if serie_number:
+            queryset = queryset.filter(serie_number__icontains=serie_number)
+
+        if category:
+            queryset = queryset.filter(category__id=category)
+
+        if brand:
+            queryset = queryset.filter(brand__id=brand)
+
         return queryset
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
