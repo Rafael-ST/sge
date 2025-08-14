1a9eec8d9e69b6dc0f42f1e8d944b6b4c7d761f1
Rafael Santos <rafaelsantost87@gmail.com>
2025-08-14 15:31:51 -0300

    metricas de vendas e gráficos

--- outflows/views.py
+++ outflows/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView, DetailView
 from . import models, forms
 from django.urls import reverse_lazy
+from app import metrics
 
 
 class OutflowListView(ListView):
@@ -18,8 +19,13 @@ class OutflowListView(ListView):
 
         if product:
             queryset = queryset.filter(product__title__icontains=product)
-        
+
         return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sales_metrics'] = metrics.get_sales_metrics()
+        return context
 
 
 class OutflowCreateView(CreateView):
